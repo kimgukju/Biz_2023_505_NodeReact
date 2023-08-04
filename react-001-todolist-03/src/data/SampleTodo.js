687c389c9431926de92f382945ea0274fc1f44f0
kimgukju <kimgukju@kimgukju.com>
2023-08-04 09:21:02 +0900

    todo-03

---
+++ react-001-todolist-03/src/data/SampleTodo.js
@@ -0,0 +1,39 @@
+const SampleData = [
+  {
+    id: 0,
+    sdate: "2023-08-03",
+    stime: "10:00:00",
+    content: "React 학습하기",
+    complete: true,
+  },
+  {
+    id: 1,
+    sdate: "2023-08-03",
+    stime: "10:20:00",
+    content: "TodoList",
+    complete: true,
+  },
+  {
+    id: 2,
+    sdate: "2023-08-03",
+    stime: "10:30:00",
+    content: "목요일은 시간이 안간다",
+    complete: false,
+  },
+  {
+    id: 3,
+    sdate: "2023-08-03",
+    stime: "10:40:00",
+    content: "숨쉬기",
+    complete: false,
+  },
+  {
+    id: 4,
+    sdate: "2023-08-03",
+    stime: "10:50:00",
+    content: "토요일은 빈둥대기",
+    complete: false,
+  },
+];
+
+export { SampleData };
