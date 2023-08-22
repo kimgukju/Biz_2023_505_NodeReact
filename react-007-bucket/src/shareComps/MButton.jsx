632cc38fa60149ae09e3fb73d2d1b3eef2997f0c
kimgukju <kimgukju@kimgukju.com>
2023-08-22 10:26:24 +0900

    0822

---
+++ react-007-bucket/src/shareComps/MButton.jsx
@@ -0,0 +1,8 @@
+import style from "./MButton.module.css";
+
+const Button = ({ children, type }) => (
+  <button className={style.btn} type={type}>
+    {children}
+  </button>
+);
+export default Button;
